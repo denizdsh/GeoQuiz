51b5a025604afafa0776fa04fbcba2e45eda5ddf
Deniz Shyukryuev <denizztto@gmail.com>
2022-01-20 03:37:42 +0200

    styling

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -1,18 +1,16 @@
 import './GameCard.css'
 
 export default function GameCard({ data }) {
-
     return (
-        <section className="card">
+        <section className='card'>
             <article className="card-img">
                 <img src={data.image} alt={data.title} />
             </article>
             <article className='card-btn-wrapper'>
-                <button class="card-btn" type="button">
+                <button className='card-btn' type="button">
                     {data.title}
                 </button>
-
             </article>
-        </section>
+        </section >
     )
 }
